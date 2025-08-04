13f4843aa51b89c6a884a85921eebe10b49e80b7
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-08-04 07:45:34 +0100

    Update backend link

--- src/views/Disease.tsx
+++ src/views/Disease.tsx
@@ -1,8 +1,9 @@
 export const fetchDiseaseData = async (): Promise<any[]> => {
-  const response = await fetch("http://127.0.0.1:5000/fetch-data");
+  const response = await fetch("https://hanover-backend.onrender.com/fetch-data");
   if (!response.ok) {
     throw new Error("Failed to fetch disease data");
   }
   const data = await response.json();
   return data;
 };
+

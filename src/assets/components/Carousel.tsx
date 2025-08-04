e678529cadbe36120909cd8850e880107714512c
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-08-04 03:52:21 +0100

    Update Carousel.tsx

--- src/assets/components/Carousel.tsx
+++ src/assets/components/Carousel.tsx
@@ -21,33 +21,36 @@ export default function Carousel() {
           <Link to={"/info/hepatitis_b"}>
             <img src={"/img/malaria.jpg"} alt="hepatitis" className="w-full h-60 m-auto object-cover" />
             <h3 className="my-3">Hepatitis B</h3>
-            <p className="line-clamp-3">{desc}</p>
+            <p className="line-clamp-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula 
+                        ante eget aliquam venenatis. Donec congue at ligula in cursus</p>
           </Link>
         </SplideSlide>
         <SplideSlide className="p-5">
           <Link to={"/info/hiv"}>
             <img src={"/img/malaria.jpg"} alt="hiv" className="w-full h-60 m-auto object-cover" />
             <h3 className="my-3">HIV</h3>
-            <p className="line-clamp-3">{desc}</p>
+            <p className="line-clamp-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula 
+                        ante eget aliquam venenatis. Donec congue at ligula in cursus</p>
           </Link>
         </SplideSlide>
         <SplideSlide className="p-5">
           <Link to={"/info/malaria"}>
             <img src={"/img/malaria.jpg"} alt="malaria" className="w-full h-60 m-auto object-cover" />
             <h3 className="my-3">Malaria</h3>
-            <p className="line-clamp-3">{desc}</p>
+            <p className="line-clamp-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula 
+                        ante eget aliquam venenatis. Donec congue at ligula in cursus</p>
           </Link>
         </SplideSlide>
         <SplideSlide className="p-5">
           <Link to={"/overviewpage"}>
             <img src={"/img/malaria.jpg"} alt="more" className="w-full h-60 m-auto object-cover" />
               <h3 className="my-3">More</h3>
-              <p className="line-clamp-3">{desc}</p>
           </Link>
         </SplideSlide>
         </Splide>
     )
 
 }
+
 
 

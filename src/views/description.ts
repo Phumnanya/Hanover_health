570e869c97476f01c0d9b871262b554d29746133
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2026-09-17 18:17:44 +0100

    Create description.ts

---
+++ src/views/description.ts
@@ -0,0 +1,40 @@
+//description for each disease
+  const diseaseDescriptions: Record<string, string> = {
+
+    hepatitis: "Hepatitis is an inflammation of the liver that is caused by a variety of infectious viruses and non-infectious agents leading to a range of health problems, 
+      including severe liver damage and cancer, some of which can be fatal. There are 5 main strains of the hepatitis virus, referred to as types A, B, C, D and E. While they 
+      can all cause liver disease, they differ in important ways including modes of transmission, severity of the illness, geographical distribution and prevention methods. 
+        In particular, types B and C lead to chronic disease in hundreds of millions of people and together are the most common cause of liver cirrhosis, liver cancer and viral 
+        hepatitis-related deaths. They are among the main infectious disease killers, causing an estimated 1.3 million deaths per year. An estimated 304 million people worldwide 
+          live with hepatitis B or C, and for most, testing and treatment remain beyond reach.",
+
+    hiv: "Human immunodeficiency virus (HIV) is an infection that attacks the body’s immune system, specifically the white blood cells called CD4 cells. HIV destroys these CD4 cells, 
+            weakening a person’s immunity against opportunistic infections, such as tuberculosis and fungal infections, severe bacterial infections and some cancers. WHO recommends 
+      that every person who may be at risk of HIV should access testing. People at increased risk of acquiring HIV should seek comprehensive and effective HIV prevention, testing and 
+        treatment services. HIV infection can be diagnosed using simple and affordable rapid diagnostic tests, as well as self-tests. It is important that HIV testing services follow 
+          the 5Cs: consent, confidentiality, counselling, correct results and connection with treatment and other services. ",
+      
+    malaria: "Malaria is a life-threatening disease caused by parasites that are transmitted to people through the bites of infected female Anopheles mosquitoes. It is preventable and 
+            curable. There are 5 parasite species that cause malaria in humans, and 2 of these species – Plasmodium falciparum and Plasmodium vivax – pose the greatest threat. 
+            In 2024, nearly half of the world's population was at risk of malaria. While sub-Saharan Africa carries a disproportionately high share of the global malaria burden, 
+            the WHO Regions of South-East Asia and Eastern Mediterranean, Western Pacific, and the Americas also report significant numbers of cases. There were an estimated 282 million 
+            cases of malaria in 2024, and the estimated number of malaria deaths stood at 610 000. In 2024, the African Region was home to 94% and 95% of malaria cases and deaths, 
+              respectively. Children under 5 years of age are most at risk of severe complications from malaria; in 2024, they accounted for nearly 75% of all malaria deaths in the 
+              WHO African Region.",
+  
+    yellow fever: "Yellow fever is a viral disease that is transmitted to humans by the bites of infected mosquitoes. It is prone to epidemics and is preventable with a vaccine. 
+      These day-biting mosquitoes breed around houses (domestic), in forests or jungles (wild), or in both habitats (semi-domestic). Yellow fever is a high-impact, high-threat disease, 
+      with a risk of international spread, representing a potential threat to global health security. Occasionally, infected travellers have exported cases to countries that are free 
+      of yellow fever. However, the disease can only spread easily to a new country if there are mosquito species able to transmit it, specific climatic conditions, and the animal 
+        reservoir needed to maintain it.",
+
+    tuberculosis: "TB is caused by bacteria (Mycobacterium tuberculosis) and it most often affects the lungs. TB is spread through the air when people with lung TB cough, sneeze or spit. 
+      A person needs to inhale only a few germs to become infected. Every year, 10 million people fall ill with tuberculosis (TB). Despite being a preventable and curable disease, 
+    1.5 million people die from TB each year – making it the world’s top infectious killer. TB is the leading cause of death of people with HIV and also a major contributor to antimicrobial 
+    resistance. About a quarter of the global population is estimated to have been infected with TB bacteria, but most people will not go on to develop TB disease and some will 
+      clear the infection. Those who are infected but not (yet) ill with the disease cannot transmit it. People infected with TB bacteria have a 5–10% lifetime risk of falling ill with TB. 
+        Those with compromised immune systems, such as people living with HIV, malnutrition or diabetes, or people who use tobacco, have a higher risk of falling ill.",
+};
+
+export default diseaseDescriptions;
+  }
